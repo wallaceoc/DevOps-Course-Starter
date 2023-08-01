e8d802e044a630c7f179587669b89e6fb1327f40
Billy O'Connell <billy.oconnell@gmail.com>
2023-08-01 15:18:40 +0100

    Add exercise changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_item, get_items, add_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +9,33 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo = get_items()
+    print(todo)
+    todo = sorted(todo, key=lambda x: x['status'], reverse=True)
+    print(todo)
+    return render_template('index.html', todo=todo)
+
+@app.route('/todo/submit', methods=['POST'])
+def add_todo():
+    add_item(request.form.get('todo-title'))
+
+    return redirect(url_for('index'))
+
+@app.route('/todo/update', methods=['POST'])
+def update_todo():
+    todo = get_items()
+
+    for item in todo:
+        form_item = dict(request.form)
+        id = str(item['id'])
+        if id in form_item:            
+            item['status'] = 'Completed'
+        else:            
+            item['status'] = 'Not Started'
+
+        save_item(item)
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
