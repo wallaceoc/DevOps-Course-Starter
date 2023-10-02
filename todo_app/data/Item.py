e9aae90819eb462b3aefc7745414f53db43e8401
Billy O'Connell <billy.oconnell@gmail.com>
2023-10-02 12:03:25 +0100

    updates based on feedback from Pull Request

--- todo_app/data/Item.py
+++ todo_app/data/Item.py
@@ -11,12 +11,10 @@ class Item:
 
     @classmethod
     def from_trello_card(cls, card):
-        completed_list_id = os.getenv('CORNDEL_TODO_LIST_ID')
+        todo_list_id = os.getenv('CORNDEL_TODO_LIST_ID')
         trello_idList = card['idList']
-        if(trello_idList == completed_list_id):
-            return cls(card['id'], card['name'], card['desc'], trello_idList, card['due'])
-        else:
-            return cls(card['id'], card['name'], card['desc'], trello_idList, card['due'], 'Done')
+        status = "To Do" if trello_idList == todo_list_id else "Done"
+        return cls(card['id'], card['name'], card['desc'], trello_idList, card['due'], status)
         
     def datePickerDueDate(self):
         if(self.dueDate is None):
